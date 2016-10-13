dfa0b3671f7a5749e13a79cdb5a9b6867096220a
Juan-Hernandez7 <Juanhernandez3119@gmail.com>
2016-10-13 11:34:24 -0700

    named cow undefined otherswise complete

--- NamedCow.java
+++ NamedCow.java
@@ -1,4 +1,18 @@
-class NamedCow
+class NamedCow extends Cow
 {
+  public Cow(String type, String name,String sound)  
+        {         
+         myType = type;         
+         mySound = sound;
+         myName = name;     
+        }     
 
+     public cow()
+     {
+
+     	myType= "cow";
+     	myName="Elsie";
+     	mySound= "moo"
+     }
+     public String getName(){return myName;}
 }
--- OldMacDonald.pde
+++ OldMacDonald.pde
@@ -1,6 +1,8 @@
 
 void setup()
-{
-	//your variable declarations here
-}
+{    Farm animalSounds = new Farm();
+	 Cow c = new Cow("cow", "moo");   
+    System.out.println(c.getType() + " goes " + c.getSound());    
+}//your variable declarations here
+
 
